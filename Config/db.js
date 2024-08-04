e239fc8341422d20a38bb8bba076c2943cc0381a
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2024-08-04 17:02:43 +0530

    backend evaluation

--- Config/db.js
+++ Config/db.js
@@ -1,8 +1,3 @@
-// const mongoose=require("mongoose")
-// const connection= mongoose.connect("mongodb://localhost:27017/guest")
-// module.exports=connection
-
-
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
